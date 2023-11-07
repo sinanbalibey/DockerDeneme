27c122aa399b83079174ebd53ee2cb512736f92b
Sinan Balıbey <sinanbey23@hotmail.com>
2023-11-07 10:02:30 +0300

    DockerDeneme

---
+++ DockerDeneme/DockerDeneme/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DockerDeneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(DateTime.Now.Second);
+                Console.Write("İntellium SOFTWARE");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
